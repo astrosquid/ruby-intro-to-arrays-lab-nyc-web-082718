0a442bf22758bd43211cec7639e710806b232ef4
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 20:26:57 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,3 +18,14 @@ def last_element(a)
   a.last 
 end 
 
+def first_element_with_array_methods(a)
+  a.first 
+end 
+
+def last_element_with_array_methods(a)
+  a.last 
+end 
+
+def length_of_array(a)
+  a.size 
+end 
